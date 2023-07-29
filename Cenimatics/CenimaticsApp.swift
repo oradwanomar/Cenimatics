3da22f3c18f981612e217bcd75a35b837f02991c
Omar R <omar.ahmedradwan@vodafone.com>
2023-07-29 18:27:36 +0300

    Initial Commit

---
+++ Cenimatics/CenimaticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CenimaticsApp.swift
+//  Cenimatics
+//
+//  Created by Omar Radwan on 29/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CenimaticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
